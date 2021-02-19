9aa5b81112c6ab8313964efc01939db3f3c180ff
erboro97 <erboro97@gmail.com>
2021-02-19 17:09:29 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DemoProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoProject.dir/main.c.obj"
+  "DemoProject.exe"
+  "DemoProject.exe.manifest"
+  "DemoProject.lib"
+  "DemoProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DemoProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void kiir(int);
+int main() {
+    int x=2;
+    while(x<3){
+        x++;
+    }
+    kiir(x);
+    return 0;
+}
+void kiir(int x){
+    printf("%d",x);
+}
